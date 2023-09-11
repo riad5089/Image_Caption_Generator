7e9622ccaa95874ed28e5fe999749c1a616fd831
Md Abdur Sobhan Riad <93583569+riad5089@users.noreply.github.com>
2023-09-11 19:46:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
 
+# import streamlit as st
+# import numpy as np
+# import pickle
+# import tensorflow as tf
+# from tensorflow import keras
+# import keras.src as keras
+# from tensorflow import keras
+# from keras.models import Model
+# from keras.applications.vgg16 import VGG16, preprocess_input
+# from tensorflow.keras.preprocessing.image import load_img, img_to_array
+# from tensorflow.keras.models import load_model
+
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from PIL import Image
 import streamlit as st
 import numpy as np
 import pickle
 import tensorflow as tf
 from tensorflow import keras
-import keras.src as keras
-from tensorflow import keras
 from keras.models import Model
 from keras.applications.vgg16 import VGG16, preprocess_input
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.models import load_model
-
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 from PIL import Image
+import requests
+from io import BytesIO
+import pyttsx3
+import base64
+
 import requests
 from io import BytesIO
 import pyttsx3
